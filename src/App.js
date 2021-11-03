9abb70a500cdd03907d3b16b26b885d7d80a3541
Tarunkcode <68896998+Tarunkcode@users.noreply.github.com>
2021-11-03 22:31:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-
-import HomePage from './pages/homepage/homepage.component.jsx';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component.jsx";
 import "./pages/homepage/homepage.styles.scss";
-function App(){
-  return (<div>
-  <HomePage />
+// import HatsPage from "./pages/homepage/homepage.component";
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
   </div>
+);
+function App() {
+  return (
+    <div>
+      <Switch>
+        {/* Switch to stop us or keep control to not render multiple components at same time accidently */}
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
   );
 }
 
