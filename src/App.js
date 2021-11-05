27b3d2b010e41c95ca3369d91439d8bdaed067bd
Tarunkcode <68896998+Tarunkcode@users.noreply.github.com>
2021-11-05 20:26:32 +0530

    header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component.jsx";
-import "./pages/homepage/homepage.styles.scss";
+// import "./pages/homepage/homepage.styles.scss";
 import ShopPage from "./pages/shop/shop.component.jsx";
-
+import Header from "./components/header/header.component";
+import "./styles.css";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         {/* Switch to stop us or keep control to not render multiple components at same time accidently */}
         <Route exact path="/" component={HomePage} />
