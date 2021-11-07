f04094dac4565fa1ca36b5f5d7903ca380dede8c
Tarunkcode <68896998+Tarunkcode@users.noreply.github.com>
2021-11-07 15:46:02 +0530

    sign-in/sign-up page and their sign-in, form input, custom-button component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component";
 // import "./pages/homepage/homepage.styles.scss";
-import ShopPage from "./pages/shop/shop.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SingnInAndSignUpPage from "./pages/sign-in+sign-up/sign-in+sign-up.component";
 import "./styles.css";
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         {/* Switch to stop us or keep control to not render multiple components at same time accidently */}
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SingnInAndSignUpPage} />
       </Switch>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import ReactDOM from "react-dom";
 import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import "./styles.css";
 
 ReactDOM.render(
   <BrowserRouter>
