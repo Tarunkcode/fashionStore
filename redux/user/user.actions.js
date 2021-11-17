2cb0b76bf824e5149355a7ca6e12f3aeead360b3
Tarunkcode <68896998+Tarunkcode@users.noreply.github.com>
2021-11-17 15:58:43 +0530

    updating our app to redirect to home if user is signed in

--- redux/user/user.actions.js
+++ redux/user/user.actions.js
@@ -1,6 +1,7 @@
 // function that return obj ,each obj is in correct format that the action is expected to be.
+import {UserActionTypes} from './user.types' // store the state of our current User
 
 export const setCurrentUser = user => ({
-type : 'SET_CURRENT_USER',
+type : UserActionTypes.SET_CURRENT_USER,
 payload : user
 });
--- redux/user/user.reducer.js
+++ redux/user/user.reducer.js
@@ -1,10 +1,11 @@
-// store the state of our current User
+
+import {UserActionTypes} from './user.types' // store the state of our current User
 const INITIAL_STATE ={
   currentUser : null
 }
 const userReducer = (state = INITIAL_STATE, action)=> {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case UserActionTypes.SET_CURRENT_USER:
       
       return {
         ...state,
