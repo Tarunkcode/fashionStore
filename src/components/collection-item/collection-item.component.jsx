a9ad74aaac2d81544f2c763359db5a672f9e81a9
Tarunkcode <68896998+Tarunkcode@users.noreply.github.com>
2021-11-25 23:56:53 +0530

    add stylings to our collection items and add custom button to it i,e add to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import './collection-item.styles.scss';
+import React from "react";
+import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
-const CollectionItem = ({imageUrl, id, name, price}) => (
-  <div className = 'collection-item'>
-  <div className ='image' 
-  style = {{
-backgroundImage : `url(${imageUrl})`
-  }}
-  />
-  <div className='collection-footer'>
-  <span className ='name'>{name}</span>
-  <span className='price'>{price}</span>
-  
-   </div>
+const CollectionItem = ({ imageUrl, id, name, price }) => (
+  <div className="collection-item">
+    <div
+      className="image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
+    <CustomButton inverted>Add to Cart</CustomButton>
   </div>
 );
 export default CollectionItem;
